44b15908d0d8c7f0b9997c9bd32a3a886af0e4b1
Stoyan Dimitrov <stoyan1020@gmail.com>
2024-03-21 18:31:44 +0200

    feat(frontend): Add styled GoogleActionButton enhancing default SubmitButton

---
+++ frontend/src/styled/formStyles/GoogleButtonStyled.jsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+import { SubmitButtonStyled } from "./SubmitButtonStyled";
+
+export const GoogleActionButtonStyled = styled(SubmitButtonStyled)`
+  width: 20em;
+  margin: 0 auto;
+  background-color: #db4437;
+  &:hover {
+    background-color: #c1351d;
+  }
+`;
