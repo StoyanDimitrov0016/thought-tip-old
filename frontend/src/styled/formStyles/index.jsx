775dce0cc097b9188433f7a8aa48090f72b0280d
Stoyan Dimitrov <stoyan1020@gmail.com>
2024-03-21 18:32:42 +0200

    feat(frontend): Centralize exports in index for all styled form components

---
+++ frontend/src/styled/formStyles/index.jsx
@@ -0,0 +1,9 @@
+export { FormStyled } from "./FormStyled";
+export { FieldsetStyled } from "./FieldsetStyled";
+export { LegendStyled } from "./LegendStyled";
+export { InputStyled } from "./InputStyled";
+export { LabelStyled } from "./LabelStyled";
+export { SubmitButtonStyled } from "./SubmitButtonStyled";
+
+export { GoogleActionButtonStyled } from "./GoogleButtonStyled";
+export { SignInAndUpRedirectLinkStyled } from "./SignInAndUpRedirectLinkStyled";
