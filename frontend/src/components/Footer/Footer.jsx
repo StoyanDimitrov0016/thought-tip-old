617af56c59cbe5d843f7f7aaa9981df107c2f15f
Stoyan Dimitrov <stoyan1020@gmail.com>
2024-03-25 22:54:34 +0200

    chore(frontend): Import separated styled components for Footer

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,55 +1,20 @@
-import { Link } from "react-router-dom";
-import styled from "styled-components";
 import endpoints from "../../../endpoints";
-
-const FooterContainer = styled.footer`
-  width: 100%;
-  background-color: #0d0d0d;
-  color: #eff0f3;
-  position: fixed;
-  left: 0;
-  bottom: 0;
-  display: flex;
-  justify-content: center;
-  gap: 10em;
-  min-height: 60px;
-  align-items: center;
-`;
-
-const FooterList = styled.ul`
-  list-style: none;
-  padding: 0;
-  margin: 0;
-  display: flex;
-  justify-content: center;
-`;
-
-const FooterListItem = styled.li`
-  margin-right: 20px;
-  list-style: none;
-
-  &:last-child {
-    margin-right: 0;
-  }
-`;
-
-const StyledLink = styled(Link)`
-  color: #eff0f3;
-  text-decoration: none;
-  // Ensure it behaves inline for flex layout
-  display: inline-block;
-
-  &:hover {
-    text-decoration: underline;
-  }
-`;
+import {
+  FooterContainer,
+  FooterList,
+  FooterListItem,
+  StyledLink,
+  CopyrightParagraph,
+} from "./FooterStyles";
 
 const Footer = () => {
   const currentYear = new Date().getFullYear();
 
   return (
     <FooterContainer>
-      <div>&copy; {currentYear} Thought Tip</div>
+      <CopyrightParagraph>
+        &copy; {currentYear} Thought Tip
+      </CopyrightParagraph>
       <FooterList>
         <FooterListItem>
           <StyledLink to={endpoints.CONTACT}>Contact</StyledLink>
@@ -58,7 +23,9 @@ const Footer = () => {
           <StyledLink to={endpoints.ABOUT}>About</StyledLink>
         </FooterListItem>
         <FooterListItem>
-          <StyledLink to={endpoints.GETTING_STARTED}>Getting Started</StyledLink>
+          <StyledLink to={endpoints.GETTING_STARTED}>
+            Getting Started
+          </StyledLink>
         </FooterListItem>
       </FooterList>
     </FooterContainer>
