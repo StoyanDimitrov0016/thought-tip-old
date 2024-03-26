eb0713f2b958bd28ded8b08a93f1d6854c0db518
Stoyan Dimitrov <stoyan1020@gmail.com>
2024-03-26 21:04:21 +0200

    refactor(frontend): Simplify Footer with constants and standard elements

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,33 +1,21 @@
-import endpoints from "../../../endpoints";
-import {
-  FooterContainer,
-  FooterList,
-  FooterListItem,
-  StyledLink,
-  CopyrightParagraph,
-} from "./FooterStyles";
+import { Link } from "react-router-dom";
+
+import { FooterContainer } from "./FooterStyles";
+import { applicationName, currentYear, footerLinks } from "../../constants";
 
 const Footer = () => {
-  const currentYear = new Date().getFullYear();
-
   return (
     <FooterContainer>
-      <CopyrightParagraph>
-        &copy; {currentYear} Thought Tip
-      </CopyrightParagraph>
-      <FooterList>
-        <FooterListItem>
-          <StyledLink to={endpoints.CONTACT}>Contact</StyledLink>
-        </FooterListItem>
-        <FooterListItem>
-          <StyledLink to={endpoints.ABOUT}>About</StyledLink>
-        </FooterListItem>
-        <FooterListItem>
-          <StyledLink to={endpoints.GETTING_STARTED}>
-            Getting Started
-          </StyledLink>
-        </FooterListItem>
-      </FooterList>
+      <p>
+        &copy; {currentYear} {applicationName}
+      </p>
+      <ul>
+        {footerLinks.map((link) => (
+          <li key={link.path}>
+            <Link to={link.path}>{link.label}</Link>
+          </li>
+        ))}
+      </ul>
     </FooterContainer>
   );
 };
