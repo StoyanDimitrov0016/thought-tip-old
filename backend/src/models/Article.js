59f1287b8645077e75f22d30689e6f91e9823247
Stoyan Dimitrov <stoyan1020@gmail.com>
2024-04-03 23:27:57 +0300

    refactor(backend): Optimize Article schema with indexes, remove future properties

--- backend/src/models/Article.js
+++ backend/src/models/Article.js
@@ -7,22 +7,19 @@ const articleSchema = new mongoose.Schema(
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
       required: true,
+      index: true,
     },
     content: { type: String, required: true },
     publishedDate: { type: Date, default: Date.now },
     avgReadingTime: { type: Number, required: true },
     topic: { type: String, required: true },
-    tags: [String],
-    likes: { type: Number, default: 0 },
-    comments: [
-      {
-        authorId: { type: Schema.Types.ObjectId, ref: "User" },
-        comment: String,
-        date: { type: Date, default: Date.now },
-      },
-    ],
+    tags: [{ type: String, index: true }],
+    likesCount: { type: Number, default: 0 },
+    likedBy: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
   },
-  { timestamps: true }
+  {
+    timestamps: true,
+  }
 );
 
 const Article = mongoose.model("Article", articleSchema);
