13ba4a05aabf4bea88bcab4c5342a6479975723a
Stoyan Dimitrov <stoyan1020@gmail.com>
2024-04-05 23:51:48 +0300

    refactor(backend): Append the user, article and auth routers, and default one for every other path with error message

--- backend/src/configuration/routes.js
+++ backend/src/configuration/routes.js
@@ -1,9 +1,15 @@
-import userRoutes from "../routes/userRoutes.js";
+import userRouter from "../routes/userRoutes.js";
+import articleRouter from "../routes/articleRoutes.js";
+import authRouter from "../routes/authRoutes.js";
 
 function routesSetUp(app) {
-  app.use("/api/v1/users", userRoutes);
-  //TODO: Append article routes also when builded
-  //app.use("/api/v1/articles", articleRoutes);
+  app.use("/api/v1/user", userRouter);
+  app.use("/api/v1/articles", articleRouter);
+  app.use("/api/v1/auth", authRouter);
+
+  app.all("*", (req, res) => {
+    res.status(404).json({ message: "The endpoint does not exist." });
+  });
 }
 
 export default routesSetUp;
