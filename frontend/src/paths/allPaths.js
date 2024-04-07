48bcda218b2981514ed1eb1d45b7841aa5f9defd
Stoyan Dimitrov <stoyan1020@gmail.com>
2024-04-07 23:50:40 +0300

    fear(frontend): Create centralized points for storing all of the paths

---
+++ frontend/src/paths/allPaths.js
@@ -0,0 +1,26 @@
+const allPaths = {
+  home: "/",
+  particularArticle: (articleId) => `/articles/${articleId}`,
+  userNotifications: "/my-notifications",
+  userProfile: "/my-profile",
+  createArticle: "/create-article",
+  updateArticle: (articleId) => `/update-article/${articleId}`,
+  // Authenticate part:
+  authenticateRedirect: "/authenticate-redirect",
+  signIn: "/auth/sign-in",
+  signUp: "/auth/sign-up",
+  signOut: "/auth/sign-out",
+  // Footer part:
+  aboutUs: "/company/about-us",
+  contactUs: "/company/contact-us",
+  careers: "/company/careers",
+  gettingStarted: "/resources/getting-started",
+  faqs: "/resources/faqs",
+  privacyPolicy: "/legals/privacy-policy",
+  termsOfService: "/legals/terms-of-service",
+  cookiesPolicy: "/legals/cookies-policy",
+  // browse topics
+  browseTopics: "/topics",
+};
+
+export default allPaths;
