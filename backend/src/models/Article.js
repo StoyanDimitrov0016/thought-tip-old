202d22da33a87e8f6421586728db4162316b1031
Stoyan Dimitrov <stoyan1020@gmail.com>
2024-04-09 21:07:49 +0300

    feat(backend): Add custom error messages to mongoose model validation

--- backend/src/models/Article.js
+++ backend/src/models/Article.js
@@ -2,17 +2,20 @@ import mongoose from "mongoose";
 
 const articleSchema = new mongoose.Schema(
   {
-    title: { type: String, required: true },
+    title: { type: String, required: [true, "Article title is required"] },
     authorId: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
       required: true,
       index: true,
     },
-    content: { type: String, required: true },
+    content: { type: String, required: [true, "Content is required"] },
     publishedDate: { type: Date, default: Date.now },
     avgReadingTime: { type: Number, required: true },
-    topic: { type: String, required: true },
+    topic: {
+      type: String,
+      required: [true, "Article should be related to at least one topic"],
+    },
     tags: [{ type: String, index: true }],
     likesCount: { type: Number, default: 0 },
     likedBy: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
