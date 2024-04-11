0934bf319e75b32a8931b2ea27eafb4adbfa6e34
Stoyan Dimitrov <stoyan1020@gmail.com>
2024-04-11 22:39:48 +0300

    fear(backend): Implement the checkSessionToken middleware to the express setup

--- backend/src/configuration/express.js
+++ backend/src/configuration/express.js
@@ -1,10 +1,12 @@
 import express from "express";
 import cors from "cors";
+import checkSessionToken from "../middleware/checkUserSession.js";
 
 function expressSetUp(app) {
   app.use(cors());
   app.use(express.json());
   app.use(express.urlencoded({ extended: false }));
+  app.use(checkSessionToken);
 }
 
 export default expressSetUp;
