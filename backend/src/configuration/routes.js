7e764f4e22af10498e5aa95dff0198365735915b
Stoyan Dimitrov <stoyan1020@gmail.com>
2024-04-14 22:55:36 +0300

    refactor(backend): Update user route to "/api/v1/users" for consistency

--- backend/src/configuration/routes.js
+++ backend/src/configuration/routes.js
@@ -3,7 +3,7 @@ import articleRouter from "../routes/articleRoutes.js";
 import authRouter from "../routes/authRoutes.js";
 
 function routesSetUp(app) {
-  app.use("/api/v1/user", userRouter);
+  app.use("/api/v1/users", userRouter);
   app.use("/api/v1/articles", articleRouter);
   app.use("/api/v1/auth", authRouter);
 
