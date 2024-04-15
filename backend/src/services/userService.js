601d0753481a9d395ef0824b5877c321e72f58c0
Stoyan Dimitrov <stoyan1020@gmail.com>
2024-04-15 21:32:15 +0300

    feat(backend):  Separate authService for authentication logic and there create get author and user functions

--- backend/src/services/userService.js
+++ backend/src/services/userService.js
@@ -1,87 +1,43 @@
 import User from "../models/User.js";
-import jwt from "jsonwebtoken";
-import bcrypt from "bcrypt";
-import "dotenv/config.js";
 
-const JWT_SECRET = process.env.JWT_SECRET;
-
-const blacklist = new Set();
-
-async function signUp(firstName, lastName, email, password) {
-  const existingUser = await User.findOne({ email }).collation({
-    locale: "en",
-    strength: 2,
-  });
-
-  if (existingUser) {
-    throw new Error("Email is taken");
-  }
-
-  const hashedPassword = await bcrypt.hash(password, 10);
-
-  const user = await User.create({
-    firstName,
-    lastName,
-    email,
-    password: hashedPassword,
-  });
-
-  const token = createJWT(user);
-  return token;
-}
-
-async function signIn(email, password) {
-  const user = await User.findOne({ email }).collation({
-    locale: "en",
-    strength: 2,
-  });
-
-  if (!user) {
-    throw new Error("Incorrect user credentials");
-  }
-
-  const doPasswordsMatch = await bcrypt.compare(password, user.password);
-
-  if (!doPasswordsMatch) {
-    throw new Error("Incorrect user credentials");
-  }
-
-  const token = createJWT(user);
-  return token;
-}
-
-async function signOut(token) {
-  blacklist.add(token);
-}
-
-function createJWT({ _id, email, firstName, lastName }) {
-  const payload = {
-    _id,
-    email,
-    firstName,
-    lastName,
-  };
-
-  const token = jwt.sign(payload, JWT_SECRET, { expiresIn: "1h" });
-  return token;
-}
-
-function verifyJWT(token) {
+async function getUserProfile(userId) {
   try {
-    if (blacklist.has(token)) {
-      throw new Error("Expired token");
+    const desiredUser = await User.findById(userId).populate("articles", "title thumbnail preview avgReadingTime publishedDate");
+    if (!desiredUser) {
+      throw new Error(`User not found`);
     }
-    const decoded = jwt.verify(token, JWT_SECRET);
-    return decoded;
+    return desiredUser;
   } catch (error) {
-    throw new Error(`Token verification failed:${error}`);
+    console.error(
+      `Failed getting user profile with id: ${userId} ---error: ${error}`
+    );
+    throw error;
   }
 }
 
-const userService = {
-  signUp,
-  signIn,
-  signOut,
-  createJWT,
-  verifyJWT,
+async function getAuthorProfile(authorId) {
+  const desiredAuthor = await User.findById(authorId)
+    .select("firstName lastName profilePicture")
+    .populate(
+      "articles",
+      "title thumbnail preview avgReadingTime publishedDate"
+    );
+
+  return desiredAuthor;
+}
+
+async function updateUserProfile(userId, updatedData) {
+  const updatedUser = await User.findByIdAndUpdate(
+    userId,
+    { $set: updatedData },
+    { new: true, runValidators: true }
+  );
+
+  return updatedUser;
+}
+
+export const userService = {
+  getUserProfile,
+  getAuthorProfile,
+  updateUserProfile,
 };
