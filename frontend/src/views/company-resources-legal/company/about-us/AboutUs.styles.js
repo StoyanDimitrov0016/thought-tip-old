5662106e660e80174becf9fa142cabfa74b0c974
Stoyan Dimitrov <stoyan1020@gmail.com>
2024-04-20 20:34:31 +0300

    feat(frontend): Create the styles for the AboutUs view component

---
+++ frontend/src/views/company-resources-legal/company/about-us/AboutUs.styles.js
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+
+export const MainSection = styled.section`
+  padding: 1.25em;
+  background: var(--background);
+`;
+
+export const Header = styled.header`
+  text-align: center;
+`;
+
+export const Title = styled.h1`
+  font-size: 2em;
+  color: var(--text);
+`;
+
+export const Description = styled.p`
+  font-size: 1.2em;
+  color: var(--secondary);
+`;
+
+export const ContentSection = styled.section`
+  margin-top: 1.25em;
+`;
+
+export const SectionHeader = styled.h2`
+  font-size: 1.5em;
+  color: var(--primary);
+`;
+
+export const SectionContent = styled.p`
+  font-size: 1.1em;
+  color: var(--secondary);
+`;
