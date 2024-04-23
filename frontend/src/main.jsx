b383bada6b05b50010283f7355ffa220432f09d7
Stoyan Dimitrov <stoyan1020@gmail.com>
2024-04-23 21:28:47 +0300

    feat(frontend): Wrap the app component with Browser Router and User Provider context hook

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,10 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import GlobalStyle from "./global.styles.js";
+import { UserProvider } from "./contexts/UserContext.jsx";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <GlobalStyle />
-    <App />
+
+    <BrowserRouter>
+      <UserProvider>
+        <App />
+      </UserProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
