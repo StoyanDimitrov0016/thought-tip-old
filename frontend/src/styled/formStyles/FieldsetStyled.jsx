5280738a780cde01d92af718fd3e75d356bc6809
Stoyan Dimitrov <stoyan1020@gmail.com>
2024-04-24 18:10:19 +0300

    delete(frontend): Remove the old form styles

--- frontend/src/styled/formStyles/FieldsetStyled.jsx
+++
@@ -1,7 +0,0 @@
-import styled from "styled-components";
-
-export const FieldsetStyled = styled.fieldset`
-  border: none;
-  margin: 0;
-  padding: 0;
-`;
--- frontend/src/styled/formStyles/LegendStyled.jsx
+++
@@ -1,6 +0,0 @@
-import styled from "styled-components";
-
-export const LegendStyled = styled.legend`
-  font-size: 1.5rem;
-  margin-bottom: 1rem;
-`;
