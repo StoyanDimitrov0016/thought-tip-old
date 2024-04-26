f3a3e9f2567cef9772a1fd1c6ec702649631d775
Stoyan Dimitrov <stoyan1020@gmail.com>
2024-04-26 23:22:19 +0300

    feat(frontend): Create the predefined categories for create article form

---
+++ frontend/src/components/forms/create-article/categories.js
@@ -0,0 +1,92 @@
+export const categories = [
+  {
+    id: 1,
+    value: "healthAndWellness",
+    title: "Health and wellness",
+  },
+  {
+    id: 2,
+    value: "technology",
+    title: "Technology",
+  },
+  {
+    id: 3,
+    value: "gadgets",
+    title: "Gadgets",
+  },
+  {
+    id: 4,
+    value: "businessAndFinance",
+    title: "Business and finance",
+  },
+  {
+    id: 5,
+    value: "travelAndTourism",
+    title: "Travel and tourism",
+  },
+  {
+    id: 6,
+    value: "mustReadBooks",
+    title: "Top Must-Read Books",
+  },
+  {
+    id: 7,
+    value: "lifestyleAndFashion",
+    title: "Lifestyle and fashion",
+  },
+  {
+    id: 8,
+    value: "environmentAndSustainability",
+    title: "Environment and sustainability",
+  },
+  {
+    id: 9,
+    value: "educationAndCareer",
+    title: "Education and career",
+  },
+  {
+    id: 10,
+    value: "politicsAndSociety",
+    title: "Politics and society",
+  },
+  {
+    id: 11,
+    value: "personalDevelopment",
+    title: "Personal development and self-improvement",
+  },
+  {
+    id: 12,
+    value: "sportsAndEntertainment",
+    title: "Sports and entertainment",
+  },
+  {
+    id: 13,
+    value: "artsAndCulture",
+    title: "Arts and culture",
+  },
+  {
+    id: 14,
+    value: "foodAndCooking",
+    title: "Food and cooking",
+  },
+  {
+    id: 15,
+    value: "newsAndCurrentEvents",
+    title: "News and current events",
+  },
+  {
+    id: 16,
+    value: "historyAndSociety",
+    title: "History and society",
+  },
+  {
+    id: 17,
+    value: "remoteWork",
+    title: "Remote work and productivity",
+  },
+  {
+    id: 18,
+    value: "trendingTopics",
+    title: "Trending Topics",
+  },
+];
