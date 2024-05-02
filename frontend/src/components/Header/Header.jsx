4c13b9be683c8eac215eb6822ea6314282e35cdf
Stoyan Dimitrov <stoyan1020@gmail.com>
2024-05-02 22:24:32 +0300

    refac(frontend): Separate the styled components from the .jsx file

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,27 +1,12 @@
-import styled from "styled-components";
-import MainNavigationBar from "./MainNavigationBar";
-
-const HeaderStyled = styled.header`
-  width: 100%;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  background-color: #0d0d0d;
-  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.05);
-  position: sticky;
-  top: 0;
-  z-index: 100;
-
-  @media (max-width: 768px) {
-    flex-direction: column;
-  }
-`;
+import { HeaderContainer } from "../article-preview-card/article-preview-card.styles";
+import NavigationBar from "./NavigationBar/NavigationBar";
 
 const Header = () => {
   return (
-    <HeaderStyled>
-      <MainNavigationBar />
-    </HeaderStyled>
+    <HeaderContainer>
+      <NavigationBar />
+      <hr />
+    </HeaderContainer>
   );
 };
 
