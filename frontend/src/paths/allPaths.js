167ca17f2b31e6fe74cf137ff135b811283496c2
Stoyan Dimitrov <stoyan1020@gmail.com>
2024-05-07 22:56:25 +0300

    feat(frontend): Add user details, articles, and favorites paths

--- frontend/src/paths/allPaths.js
+++ frontend/src/paths/allPaths.js
@@ -2,14 +2,22 @@ const allPaths = {
   home: "/",
   particularArticle: (articleId) => `/articles/${articleId}`,
   userNotifications: "/my-notifications",
-  userProfile: "/my-profile",
+  userProfile: {
+    details: "/my-profile/details",
+    articles: "/my-profile/articles",
+    favorites: "/my-profile/favorites",
+  },
   createArticle: "/create-article",
   updateArticle: (articleId) => `/update-article/${articleId}`,
+
+  //
+
   // Authenticate part:
   authenticateRedirect: "/authenticate-redirect",
   signIn: "/auth/sign-in",
   signUp: "/auth/sign-up",
   signOut: "/auth/sign-out",
+
   // Footer part:
   aboutUs: "/company/about-us",
   contactUs: "/company/contact-us",
@@ -19,6 +27,7 @@ const allPaths = {
   privacyPolicy: "/legals/privacy-policy",
   termsOfService: "/legals/terms-of-service",
   cookiesPolicy: "/legals/cookies-policy",
+
   // browse topics
   browseTopics: "/topics",
 };
